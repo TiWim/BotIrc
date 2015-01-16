9f21e57a27dfeda2805b6ecb80fe7c5dbe7cc07b
betezed <bentz.romain@gmail.com>
2015-01-16 19:42:20 +0100

    Ajout !stat

--- bot.py
+++ bot.py
@@ -1,12 +1,6 @@
+import time
 import irclib
 import ircbot
-import random
-import urllib2
-import re
-import time
-from datetime import date
-import operator
-import hashlib
 from modules import ModBot, ModBoobies, ModStat, ModRmd5, ModMd5, utils
 
 
@@ -36,22 +30,23 @@ class BetezedBot(ircbot.SingleServerIRCBot):
         #serv.join(self.canal_test)
 
     def on_kick(self, serv, ev):
+        canal = ev.target()
         time.sleep(2)
-        serv.join(self.canal)
+        serv.join(canal)
 
     def on_pubmsg(self, serv, ev):
         handle = irclib.nm_to_n(ev.source())
         canal = ev.target()
         message = ev.arguments()[0]
-        self.mods['stat'].update_counts(handle)
+        self.mods['!stat'].update_counts(handle)
 
         for mod, value in self.mods:
             if mod in message:
-                if not self.check_flood(serv, ev, handle):
+                if not self.check_flood(serv, handle):
                     custom_message = utils.extract_message(message, mod)
                     self.mods[mod].execute(serv, canal, handle, custom_message)
 
-    def check_flood(self, serv, ev, handle):
+    def check_flood(self, serv, handle):
         self.current_time = time.time()
         if self.current_time - self.last_time < self.flood_time:
             print "Flood : " + str(self.current_time) + " - " + str(self.last_time)
