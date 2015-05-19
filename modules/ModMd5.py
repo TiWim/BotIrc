5309a60ccce88a90c9a9d0a09fd6b12d6d9ea0e4
Betezed <bentz.romain@gmail.com>
2015-05-19 11:32:53 +0200

    change

--- modules/ModMd5.py
+++ modules/ModMd5.py
@@ -1,4 +1,5 @@
 import hashlib
+import urllib2
 
 
 class ModMd5:
@@ -9,3 +10,5 @@ class ModMd5:
         m = hashlib.md5()
         m.update(message)
         serv.privmsg(canal, "md5(" + message + ") = " + m.hexdigest())
+	url = "http://md5.gromweb.com/?string=" + str(message)
+        response = urllib2.urlopen(url)
